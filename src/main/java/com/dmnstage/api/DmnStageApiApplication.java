8167e2b325b329950108bf546d9fe82f57661814
AbdellahASKI <abdellahaski@gmail.com>
2018-07-20 12:18:04 +0100

    Added User&Admin&Client Classes + application.properties config

--- src/main/java/com/dmnstage/api/DmnStageApiApplication.java
+++ src/main/java/com/dmnstage/api/DmnStageApiApplication.java
@@ -8,13 +8,7 @@ public class DmnStageApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DmnStageApiApplication.class, args);
-        /*aaaaa*/
-        /*Edit*/
-
-        /*String path;
-        String pathFormat="api.dmnstage.com/img/#Category#/#Product#/#Subproduct#/#Year#-#Month#-#Day#.jpg";
-        pathFormat=pathFormat.replace("#Category#",Subproduct.pro)*/
-
+        
 
     }
 }
