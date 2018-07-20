7a8aa4ecf064b4b52bf61406d526ed2ad7c2d50f
AbdellahASKI <abdellahaski@gmail.com>
2018-07-20 17:46:22 +0100

    Changes to Service

--- src/main/java/com/dmnstage/api/service/IService.java
+++ src/main/java/com/dmnstage/api/service/IService.java
@@ -1,5 +1,10 @@
 package com.dmnstage.api.service;
 
+import com.dmnstage.api.entities.User;
+
 public interface IService {
 
+    void newUser(User user);
+
+    void saveUser(User user);
 }
--- src/main/java/com/dmnstage/api/service/IServiceImp.java
+++ src/main/java/com/dmnstage/api/service/IServiceImp.java
@@ -1,4 +1,22 @@
 package com.dmnstage.api.service;
 
+import com.dmnstage.api.entities.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class IServiceImp implements IService {
+    @Override
+    public void newUser(User user) {
+        user.setEmail(user.getEmail().toLowerCase());
+        user.setUsername(user.getUsername().toLowerCase());
+        //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+    }
+
+    @Override
+    public void saveUser(User user) {
+
+    }
 }
