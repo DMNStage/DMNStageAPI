7873e389f04ed5509d0a3e2a862fafc49f8cefc1
AbdellahASKI <abdellahaski@gmail.com>
2018-07-20 13:21:53 +0100

    Added Produit

--- src/main/java/com/dmnstage/api/entities/Category.java
+++ src/main/java/com/dmnstage/api/entities/Category.java
@@ -14,4 +14,36 @@ public class Category implements Serializable {
     private String name;
 
     private String pathFormat;
+
+    public Category(String name, String pathFormat) {
+        this.name = name;
+        this.pathFormat = pathFormat;
+    }
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPathFormat() {
+        return pathFormat;
+    }
+
+    public void setPathFormat(String pathFormat) {
+        this.pathFormat = pathFormat;
+    }
 }
