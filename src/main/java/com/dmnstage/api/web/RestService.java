6befd378013b6dd267e059dbe5a0cf524f3ff4ce
kumohira <naitsaid.youssef@gmail.com>
2018-07-20 18:16:23 +0100

    Edited getAdmin and getAdmins

--- src/main/java/com/dmnstage/api/web/RestService.java
+++ src/main/java/com/dmnstage/api/web/RestService.java
@@ -1,6 +1,9 @@
 package com.dmnstage.api.web;
 
+import com.dmnstage.api.entities.Admin;
 import com.dmnstage.api.entities.User;
+import com.dmnstage.api.service.IService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,16 +14,17 @@ import java.util.List;
 @RestController
 public class RestService {
 
-    @RequestMapping(value = "/admin/{id}", method = RequestMethod.GET)
-    public User getStudent(@PathVariable Integer id) {
+    @Autowired //injection des dependances
+    private IService service;
 
-        return null; //studentRepository.findById(id).get()
+    @RequestMapping(value = "/admin/{id}", method = RequestMethod.GET)
+    public User getAdmin(@PathVariable Integer id) {
+        return service.getAdminById(id); //studentRepository.findById(id).get()
     }
 
     @RequestMapping(value = "/admins", method = RequestMethod.GET)
-    public List<User> getStudents() {
-
-        return null; //studentRepository.findAll()
+    public List<Admin> getAdmins() {
+        return service.getAllAdmins(); //studentRepository.findAll()
     }
 
 
