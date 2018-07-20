34556f58f3fc8aaf49de5b07090de40665932d92
AbdellahASKI <abdellahaski@gmail.com>
2018-07-20 18:04:08 +0100

    Changes to Service: getAdminById, GetAllAdmins

--- src/main/java/com/dmnstage/api/service/IService.java
+++ src/main/java/com/dmnstage/api/service/IService.java
@@ -1,10 +1,17 @@
 package com.dmnstage.api.service;
 
+import com.dmnstage.api.entities.Admin;
 import com.dmnstage.api.entities.User;
+
+import java.util.List;
 
 public interface IService {
 
     void newUser(User user);
 
     void saveUser(User user);
+
+    User getAdminById(int id);
+
+    List<Admin> getAllAdmins();
 }
--- src/main/java/com/dmnstage/api/service/IServiceImp.java
+++ src/main/java/com/dmnstage/api/service/IServiceImp.java
@@ -1,17 +1,25 @@
 package com.dmnstage.api.service;
 
+import com.dmnstage.api.entities.Admin;
 import com.dmnstage.api.entities.User;
+import com.dmnstage.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class IServiceImp implements IService {
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public void newUser(User user) {
         user.setEmail(user.getEmail().toLowerCase());
         user.setUsername(user.getUsername().toLowerCase());
-        //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 
     }
 
@@ -19,4 +27,16 @@ public class IServiceImp implements IService {
     public void saveUser(User user) {
 
     }
+
+    @Override
+    public User getAdminById(int id) {
+        return userRepository.findAdminByID(id);
+    }
+
+    @Override
+    public List<Admin> getAllAdmins() {
+        return userRepository.findAllAdmins();
+    }
+
+
 }
