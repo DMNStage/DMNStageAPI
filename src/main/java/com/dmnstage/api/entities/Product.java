4fd6c5061976bdee63875df221684199a43f6816
AbdellahASKI <abdellahaski@gmail.com>
2018-07-20 13:25:58 +0100

    Added Product Class (Changed Produit to Product)

--- src/main/java/com/dmnstage/api/entities/Product.java
+++ src/main/java/com/dmnstage/api/entities/Product.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Produit {
+public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
